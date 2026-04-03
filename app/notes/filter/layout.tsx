08de02ac7d01abb5cc9e1279c73e03b09a2491d5
Dina Muravska <dina.muravska.25@gmail.com>
2026-04-03 21:31:11 +0300

    update layout

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,10 +1,12 @@
 type Props = {
   children: React.ReactNode;
+  sidebar: React.ReactNode;
 };
 
-const NotesLayout = ({ children }: Props) => {
+const NotesLayout = ({ children, sidebar }: Props) => {
   return (
     <section>
+      <aside>{sidebar}</aside>
       <div>{children}</div>
     </section>
   );
