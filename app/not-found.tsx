5df400c9329649fff0a9fcca1dd9ef824241f406
Dina Muravska <dina.muravska.25@gmail.com>
2026-04-06 23:49:51 +0300

    fix modal and styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,14 @@
-import css from "./Home.module.css";
+import css from "./page.module.css";
 
-export default function NotFound() {
+const NotFound = () => {
   return (
-    <div>
+    <>
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.description}>
         Sorry, the page you are looking for does not exist.
       </p>
-    </div>
+    </>
   );
-}
+};
+
+export default NotFound;
